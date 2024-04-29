c027d104a6a2574a1adbfa1fff129bc9c5909d89
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2024-04-29 20:20:15 +0200

    repair routes path

--- src/components/ButtonBack.jsx
+++ src/components/ButtonBack.jsx
@@ -6,7 +6,7 @@ function ButtonBack() {
   
   
   return (
-    <NavLink to='/' className='w-12 h-12  rounded-lg bg-white items-center justify-center flex font-bold shadow-md fixed top-5 left-5 z-20'>
+    <NavLink to='/3D-portfolio/' className='w-12 h-12  rounded-lg bg-white items-center justify-center flex font-bold shadow-md fixed top-5 left-5 z-20'>
             <p className='blue-gradient_text'>Back</p>
         </NavLink>
   )
